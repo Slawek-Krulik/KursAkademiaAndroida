c18fb87f39f266825336857ebb5ce363c8970bb0
Slawomir Krulik <slawekkrulik@gmail.com>
2022-01-23 22:54:45 +0100

    Add app module

--- app/src/main/java/com/example/kursakademiaandroida/core/app/di/KoinInjector.kt
+++ app/src/main/java/com/example/kursakademiaandroida/core/app/di/KoinInjector.kt
@@ -3,5 +3,6 @@ package com.example.kursakademiaandroida.core.app.di
 import org.koin.core.module.Module
 
 val koinInjector: List<Module> = listOf(
-    networkModule
+    networkModule,
+    appModule
 )

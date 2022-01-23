71e03e4aeab8faa64cd4689af383eed0738c8e2c
Slawomir Krulik <slawekkrulik@gmail.com>
2022-01-23 23:21:19 +0100

    Add database module

---
+++ app/src/main/java/com/example/kursakademiaandroida/core/app/di/DatabaseModule.kt
@@ -0,0 +1,16 @@
+package com.example.kursakademiaandroida.core.app.di
+
+import androidx.room.Room
+import com.example.kursakademiaandroida.database.AppDatabase
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            AppDatabase::class.java,
+            "app-database"
+        ).build()
+    }
+}
---
+++ app/src/main/java/com/example/kursakademiaandroida/database/AppDatabase.kt
@@ -0,0 +1,7 @@
+package com.example.kursakademiaandroida.database
+
+import androidx.room.RoomDatabase
+
+//todo("Add @Database annotation when table structure will be know")
+abstract class AppDatabase : RoomDatabase() {
+}

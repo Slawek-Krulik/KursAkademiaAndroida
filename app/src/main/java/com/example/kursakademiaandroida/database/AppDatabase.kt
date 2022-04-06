3144081b786752b6ca963b0a022b7e5713771472
Slawomir Krulik <slawekkrulik@gmail.com>
2022-04-06 21:17:23 +0200

    add abstract method characterDao() to AppDatabase

--- app/src/main/java/com/example/kursakademiaandroida/database/AppDatabase.kt
+++ app/src/main/java/com/example/kursakademiaandroida/database/AppDatabase.kt
@@ -3,11 +3,14 @@ package com.example.kursakademiaandroida.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.kursakademiaandroida.features.characters.data.local.CharacterDao
+import com.example.kursakademiaandroida.features.characters.data.local.model.CharacterCached
 import com.example.kursakademiaandroida.features.episodes.data.local.EpisodeDao
 import com.example.kursakademiaandroida.features.episodes.data.local.model.EpisodeCached
 
-@Database(entities = [EpisodeCached::class], version = 1)
+@Database(entities = [EpisodeCached::class, CharacterCached::class], version = 1)
 @TypeConverters(ListConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun episodeDao(): EpisodeDao
+    abstract fun characterDao(): CharacterDao
 }

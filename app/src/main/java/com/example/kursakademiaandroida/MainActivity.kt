9e62b20887f44524bfa7b5107ba0fd8ae29a96ef
Slawomir Krulik <slawekkrulik@gmail.com>
2022-04-13 23:00:54 +0200

    Add bottom navigation view

--- app/src/main/java/com/example/kursakademiaandroida/MainActivity.kt
+++ app/src/main/java/com/example/kursakademiaandroida/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.kursakademiaandroida
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.kursakademiaandroida.features.characters.presentation.CharacterFragment
+import com.example.kursakademiaandroida.features.episodes.presentation.EpisodeFragment
+import com.example.kursakademiaandroida.features.locations.presentation.LocationFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setBottomNavigation()
+        showFragment(LocationFragment())
+    }
+
+    private fun setBottomNavigation() {
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.nav_view)
+        bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_location -> {
+                    showFragment(LocationFragment())
+                    true
+                }
+                R.id.nav_episode -> {
+                    showFragment(EpisodeFragment())
+                    true
+                }
+                R.id.nav_character -> {
+                    showFragment(CharacterFragment())
+                    true
+                }
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+
     }
 }
+
+
